16df5d548fd42dc5861e7f82739e5dc7b23994ae
Renan Araújo <awkrenan@gmail.com>
2024-01-28 14:13:13 -0300

    Colorful search results
    Search results now are colored!

--- rusty/src/document.rs
+++ rusty/src/document.rs
@@ -17,7 +17,7 @@ impl Document {
         let mut rows = Vec::new();
         for value in contents.lines() {
             let mut row = Row::from(value);
-            row.highlight();
+            row.highlight(None);
             rows.push(row);
         }
         Ok(Self {
@@ -50,8 +50,8 @@ impl Document {
         #[allow(clippy::indexing_slicing)]
         let current_row = &mut self.rows[at.y];
         let mut new_row = current_row.split(at.x);
-        current_row.highlight();
-        new_row.highlight();
+        current_row.highlight(None);
+        new_row.highlight(None);
         #[allow(clippy::integer_arithmetic)]
         self.rows.insert(at.y + 1, new_row);
     }
@@ -68,12 +68,12 @@ impl Document {
         if at.y == self.len() {
             let mut row = Row::default();
             row.insert(0, c);
-            row.highlight();
+            row.highlight(None);
             self.rows.push(row);
         } else {
             #[allow(clippy::indexing_slicing)]
             let row = &mut self.rows[at.y];
-            row.highlight();
+            row.highlight(None);
             row.insert(at.x, c);
         }
     }
@@ -89,11 +89,11 @@ impl Document {
             let next_row = self.rows.remove(at.y + 1);
             let row = &mut self.rows[at.y];
             row.append(&next_row);
-            row.highlight();
+            row.highlight(None);
         } else {
             let row = &mut self.rows[at.y];
             row.delete(at.x);
-            row.highlight();
+            row.highlight(None);
         }
     }
 
@@ -107,6 +107,12 @@ impl Document {
             self.dirty = false;
         }
         Ok(())
+    }
+
+    pub fn highlight(&mut self, word: Option<&str>) {
+        for row in &mut self.rows {
+            row.highlight(word);
+        }
     }
 
     pub fn is_dirty(&self) -> bool {
--- rusty/src/editor.rs
+++ rusty/src/editor.rs
@@ -152,6 +152,7 @@ impl Editor {
                     } else if moved {
                         editor.move_cursor(Key::Left);
                     }
+                    editor.document.highlight(Some(query));
                 },
             )
             .unwrap_or(None);
@@ -159,6 +160,7 @@ impl Editor {
             self.cursor_position = old_position;
             self.scroll();
         }
+        self.document.highlight(None);
     }
 
     fn process_keypress(&mut self) -> Result<(), std::io::Error> {
